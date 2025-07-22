9070fc1ed9f1328fe9ce8646103c2f2eada4b698
BatuhanOkur <batuhanokur2209@gmail.com>
2025-07-22 21:34:10 +0300

    Base middleware created and the workload of classes has been reduced by splitting them. When Handler is used, HandlerRequestResponseLoggingMiddleware will be triggered.

---
+++ BO.RequestResponseMiddleware.Library/Middlewares/HandlerRequestResponseLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using BO.RequestResponseMiddleware.Library.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO.RequestResponseMiddleware.Library.Middlewares
+{
+    internal class HandlerRequestResponseLoggingMiddleware : BaseRequestResponseMiddleware
+    {
+        private readonly Func<RequestResponseContext, Task> reqResHandler;
+
+        public HandlerRequestResponseLoggingMiddleware(RequestDelegate next, Func<RequestResponseContext, Task> reqResHandler)
+            :base(next)
+        {
+            this.reqResHandler = reqResHandler;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var reqResContext = await BaseMiddlewareInvoke(context);
+            await reqResHandler.Invoke(reqResContext);
+        }
+    }
+}
--- BO.RequestResponseMiddleware.Library/Middlewares/RequestResponseLoggingMiddleware.cs
+++ BO.RequestResponseMiddleware.Library/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -11,78 +11,20 @@ using System.Threading.Tasks;
 
 namespace BO.RequestResponseMiddleware.Library.Middlewares
 {
-    public class RequestResponseLoggingMiddleware
+    public class RequestResponseLoggingMiddleware: BaseRequestResponseMiddleware
     {
-        private readonly RequestDelegate next;
-        private readonly RequestResponseOptions reqResOptions;
-        private readonly RecyclableMemoryStreamManager recyclableMemoryStreamManager;
+        
+        
 
         public RequestResponseLoggingMiddleware(RequestDelegate next, RequestResponseOptions reqResOptions)
+            :base(next)
         {
-            this.next = next;
-            this.reqResOptions = reqResOptions;
-            recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
+            
         }
 
         public async Task Invoke(HttpContext context)
         {
-            var requestBody = await GetRequestBody(context);
-
-            var originalBodyStream = context.Response.Body;
-            await using var responseBody = recyclableMemoryStreamManager.GetStream();
-            context.Response.Body = responseBody;
-
-            var sw = Stopwatch.StartNew();
-            await next(context);
-            sw.Stop();
-
-            context.Response.Body.Seek(0, SeekOrigin.Begin);
-
-            string responseBodyText = await new StreamReader(context.Response.Body).ReadToEndAsync();
-
-            context.Response.Body.Seek(0, SeekOrigin.Begin);
-
-
-            var reqResContext = new RequestResponseContext(context)
-            {
-                ResponseCreationTime = TimeSpan.FromTicks(sw.ElapsedTicks),
-                RequestBody = requestBody,
-                ResponseBody = responseBodyText
-            };
-
-            reqResOptions.ReqResHandler?.Invoke(reqResContext);
-        }
-
-        private static string ReadStreamInChunks(Stream stream)
-        {
-            const int readChunkBufferLength = 4096;
-
-            stream.Seek(0, SeekOrigin.Begin);
-
-            using var textWriter = new StringWriter();
-            using var reader = new StreamReader(stream, Encoding.UTF8);
-
-            var readChunk = new char[readChunkBufferLength];
-            int readChunkLength;
-
-            do
-            {
-                readChunkLength = reader.ReadBlock(readChunk, 0, readChunkBufferLength);
-                textWriter.Write(readChunk, 0, readChunkLength);
-
-            }while (readChunkLength > 0);
-
-            return textWriter.ToString();
-        }
-
-        private async Task<string> GetRequestBody(HttpContext context)
-        {
-            context.Request.EnableBuffering();
-            await using var requestStream = recyclableMemoryStreamManager.GetStream();
-            await context.Request.Body.CopyToAsync(requestStream);
-            string reqBody = ReadStreamInChunks(requestStream);
-            context.Request.Body.Seek(0, SeekOrigin.Begin);
-            return reqBody;
-        }
+            await BaseMiddlewareInvoke(context);            
+        }        
     }
 }
