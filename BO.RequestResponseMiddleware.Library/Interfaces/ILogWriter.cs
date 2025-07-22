b8c39eb1821d2a217cd585980756cdb767eb3a68
BatuhanOkur <batuhanokur2209@gmail.com>
2025-07-22 21:45:42 +0300

    LoggerFactoryLogWriter created and implemented ILogWriter interface.

---
+++ BO.RequestResponseMiddleware.Library/Interfaces/ILogWriter.cs
@@ -0,0 +1,14 @@
+﻿using BO.RequestResponseMiddleware.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO.RequestResponseMiddleware.Library.Interfaces
+{
+    public interface ILogWriter
+    {
+        Task Write(RequestResponseContext context);
+    }
+}
---
+++ BO.RequestResponseMiddleware.Library/LogWriters/LoggerFactoryLogWriter.cs
@@ -0,0 +1,27 @@
+﻿using BO.RequestResponseMiddleware.Library.Interfaces;
+using BO.RequestResponseMiddleware.Library.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO.RequestResponseMiddleware.Library.LogWriters
+{
+    internal class LoggerFactoryLogWriter : ILogWriter
+    {
+        private readonly ILogger logger;
+        private readonly LoggingOptions loggingOptions;
+
+        public LoggerFactoryLogWriter(ILoggerFactory loggerFactory, LoggingOptions loggingOptions)
+        {
+            this.logger = loggerFactory.CreateLogger(loggingOptions.LoggerCategoryName);
+            this.loggingOptions = loggingOptions;
+        }
+        public Task Write(RequestResponseContext context)
+        {
+            logger.Log(loggingOptions.LogLevel, "");
+        }
+    }
+}
